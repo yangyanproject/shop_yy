ab2b0277fc625c583ec8bb62ac57824422199d45
k <492465039@qq.com>
2021-03-10 17:49:59 +0800

    提交

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '@/pages/Login'
+import Home from '@/pages/Home'
+import Search from '@/pages/Search'
+import Register from '@/pages/Register'
+
+Vue.use(VueRouter)
+
+const router= new VueRouter({
+   
+})
+export default router
